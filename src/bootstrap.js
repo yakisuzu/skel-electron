cc2576352c86ad8d391fd6d491796a1b45a8fbad
yakisuzu <yakisuzu@gmail.com>
2017-02-05 14:26:31 +0900

    work!

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,6 +1,6 @@
 'use strict';
 
 import ReactDom from 'react-dom';
-import {Main} from './components/main';
+import {Main} from './renderer/components/main';
 
 ReactDom.render(new Main().render(), document.getElementById('app'));
